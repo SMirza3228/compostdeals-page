1a483369c711ad3be76135da86ad2fc2c64e73ff
SMirza3228 <mirzaseyidov38@gmail.com>
2024-10-10 16:06:26 +0200

    two commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,45 @@ function closeSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 };
+
+const targetBlock = document.querySelector('.main__inner');
+
+const targetServers = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    })
+})
+
+targetServers.observe(targetBlock);
+
+const tagetServicesBlock = document.querySelector('.services__inner');
+
+const block = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    })
+})
+
+block.observe(tagetServicesBlock);
+
+const targetStart = document.querySelector('.start__inner');
+
+const start = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    })
+})
+
+start.observe(targetStart);
